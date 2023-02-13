589b4e778772f07904da9bbd6d41ff48a66870e2
Ryan McDonald <ryandennismcdonald@gmail.com>
2023-02-13 10:04:36 +0400

    proof of concept

--- docassemble/yoco/pay.py
+++ docassemble/yoco/pay.py
@@ -1,33 +1,46 @@
 import json
 import requests
-
+from docassemble.base.util import get_config, log
+import psycopg2
 # Anonymous test key. Replace with your key.
 SECRET_KEY = 'sk_test_960bfde0VBrLlpK098e4ffeb53e1'
-from docassemble.base.util import get_config
-import psycopg2
+
 
 def get_conn():
+    # Connect to an existing database
+    # Fetch the database details from the configuration file. Change name and options for live. 
     dbconfig = get_config('demo db')
-    return psycopg2.connect(database=dbconfig.get('postgres'),
-                            user=dbconfig.get('dbmasteruser'),
-                            password=dbconfig.get('JwxHzA7|peE_^ShMgtYt9VF1Cw!QM&$W'),
-                            host=dbconfig.get('ls-0c9c784ed8fbf9b6b0c8020bbd998255fede62d8.c68on83ma44g.eu-west-1.rds.amazonaws.com'),
-                            port=dbconfig.get('5432'),
-                            options="-c search_path=dbo,payment"),
+    return psycopg2.connect(database=dbconfig.get('name'),
+                            user=dbconfig.get('user'),
+                            password=dbconfig.get('password'),
+                            host=dbconfig.get('host'),
+                            port=dbconfig.get('port'),
+                            options="-c search_path=ideal,public")
 
-def successful_charge(id, response):
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("update Failed_Transactions set Charge_Response=%s", (response))
-    conn.commit()
-    cur.close()
 
-def failed_charge(id, response):
+def write_response_to_db(response):
     conn = get_conn()
+    # Open a cursor to perform database operations
     cur = conn.cursor()
-    cur.execute("update foo set bar=%s where id=%s", (response, id))
+    # Pass data to fill a query placeholders and let Psycopg perform
+    # the correct conversion (no more SQL injections!)
+    log(response.ok)
+    if response.ok:
+        log("Successful response")
+        postgres_insert_query = """ INSERT INTO "SuccessfulPayments" (fun, bomb) VALUES (%s, %s)"""
+        record_to_insert = ("hello", "toad")
+        cur.execute(postgres_insert_query, record_to_insert)
+    else:
+        log("Unsuccessful response")
+        cur.execute("INSERT INTO SuccessfulPayments (StatusCode) VALUES (%s)",
+        ("world"))     
+    # Make the changes to the database persistent
     conn.commit()
+    # Close communication with the database
     cur.close()
+    conn.close()
+
+
 
 def yoco_charge(token, amount):
     response = requests.post(
@@ -40,12 +53,20 @@ def yoco_charge(token, amount):
                                         'amountInCents': amount,
                                         'currency': 'ZAR',
                                     },
-                                )       
-    data = response
-    if data.status_code == '201':
-        successful_charge(data.json()['status'])
-    return data
+                                ) 
+    log(response)      
+    log(response.text)
+    log(response.json())
+    log(response.ok)
+    write_response_to_db(response)
+    return response
 
 
   # response.status_code will contain the HTTP status code 
   # response.json() will contain the response body
+
+#   Lessons: 
+
+#   With table names and column names, these will automatically be interpreted 
+#   as lower case. Need to use double quotation marks to get around this. 
+#   For example SuccessfulPayments needs to be "SuccessfulPayments"
