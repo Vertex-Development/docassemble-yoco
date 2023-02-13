6fa1df955e32f75ca7ff8715bc545a7d100a4287
Matthew Cornfield <matcornfield@gmail.com>
2023-02-13 12:37:07 +0200

    payment working

--- docassemble/yoco/pay.py
+++ docassemble/yoco/pay.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from docassemble.base.util import get_config, log
+from docassemble.base.util import get_config, log, create_session
 import psycopg2
 # Anonymous test key. Replace with your key.
 SECRET_KEY = 'sk_test_960bfde0VBrLlpK098e4ffeb53e1'
@@ -15,10 +15,16 @@ def get_conn():
                             password=dbconfig.get('password'),
                             host=dbconfig.get('host'),
                             port=dbconfig.get('port'),
-                            options="-c search_path=ideal,public")
+                            options="-c search_path=payment")
 
 
 def write_response_to_db(response):
+
+    j_response = response.json()
+
+    interview_filename = 'https://dev.platofy.com/interview?i=docassemble.playground1pay:pay.yml'
+    session_id = create_session(interview_filename)
+
     conn = get_conn()
     # Open a cursor to perform database operations
     cur = conn.cursor()
@@ -27,16 +33,42 @@ def write_response_to_db(response):
     log(response.ok)
     if response.ok:
         log("Successful response")
-        postgres_insert_query = """ INSERT INTO "SuccessfulPayments" (fun, bomb) VALUES (%s, %s)"""
-        record_to_insert = ("hello", "toad")
-        cur.execute(postgres_insert_query, record_to_insert)
+        # Define the data to be inserted
+        # Define the data to be inserted
+
+        # Define the data to be inserted
+        source_data = (j_response['id'], j_response['source']['brand'], j_response['source']['maskedCard'], j_response['source']['expiryMonth'], j_response['source']['expiryYear'], j_response['source']['fingerprint'], j_response['source']['object'])
+        # Define the data to be inserted
+        metadata_data = (j_response['id'], session_id) 
+
+        if (j_response['liveMode'] == False):
+            live_mode = False
+        else:
+            live_mode = True
+
+        charge_data = (j_response['id'], j_response['object'], j_response['status'], j_response['currency'], j_response['amountInCents'], live_mode)
+
+        # Execute the INSERT statement
+        cur.execute("INSERT INTO source (id, brand, masked_card, expiry_month, expiry_year, fingerprint, object) VALUES (%s, %s, %s, %s, %s, %s, %s)", source_data)
+
+        # Execute the INSERT statement
+        cur.execute("INSERT INTO metadata (id, docassemble_session) VALUES (%s, %s)", metadata_data)
+
+        # Execute the INSERT statement
+        cur.execute("INSERT INTO charge (id, object, status, currency, amount_in_cents, live_mode) VALUES (%s, %s, %s, %s, %s, %s)", charge_data)
     else:
-        log("Unsuccessful response")
-        cur.execute("INSERT INTO SuccessfulPayments (StatusCode) VALUES (%s)",
-        ("world"))     
-    # Make the changes to the database persistent
+        log("Unsuccessful response") 
+
+        # Define the data to be inserted
+        error_data = (j_response['errorType'], j_response['errorCode'], j_response['errorMessage'], j_response['displayMessage'])
+
+        # Execute the INSERT statement
+        cur.execute("INSERT INTO payment_errors (error_type, error_code, error_message, display_message) VALUES (%s, %s, %s, %s)", error_data)
+
+    # Commit the transaction 
     conn.commit()
-    # Close communication with the database
+
+    # Close the cursor and connection
     cur.close()
     conn.close()
 
